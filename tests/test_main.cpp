1cf482a839b1900a9c56d6a21da7b914e71b4c77
Milan Veselý <vesely202@gmail.com>
2023-05-07 11:31:36 +0200

    Changed tests to depend on mounted fs

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -4,29 +4,7 @@
 
 #include "common.h"
 
-TEST(Ready, Mountpoint_set) {
-    EXPECT_NE(TestConfig::inst().mountpoint.to_string(), "");
-}
-
-class CustomTestListener : public testing::EmptyTestEventListener {
-public:
-    bool ready_failed_ = false;
-
-    void OnTestSuiteEnd(const testing::TestSuite& test_suite) override {
-        if (std::string(test_suite.name()).find("Ready") != std::string::npos && test_suite.Failed()) {
-            ready_failed_ = true;
-        }
-    }
-
-    void OnTestStart(const testing::TestInfo& test_info) override {
-        if (ready_failed_) {
-            GTEST_SKIP() << "Skipping test since no mountpoint was provided.";
-        }
-    }
-};
-
 int main(int argc, char** argv) {
-    // Parse mountpoint from command line arguments
     for (int i = 0; i < argc; i++) {
         if (std::string(argv[i]) == "--mountpoint" || std::string(argv[i]) == "-m") {
             if (i + 1 < argc) {
@@ -36,19 +14,17 @@ int main(int argc, char** argv) {
                 for (int j = i; j < argc; j++) {
                     argv[j] = argv[j + 2];
                 }
-                break;
-
-            } else {
-                throw std::runtime_error("mountpoint not found");
             }
+
+            break;
         }
     }
 
+    if (TestConfig::inst().mountpoint.to_string().empty()) {
+        throw std::runtime_error("No mountpoint provided");
+    }
+
     testing::InitGoogleTest(&argc, argv);
-
-    testing::TestEventListeners& listeners = testing::UnitTest::GetInstance()->listeners();
-    listeners.Append(new CustomTestListener);
-
     int test_results = RUN_ALL_TESTS();
 
     return test_results;

fb5860d378237f4ed78f8f8d2baa54069646a851
Milan Veselý <vesely202@gmail.com>
2023-05-08 14:29:13 +0200

    Proper github, gitlab tests

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -5,6 +5,8 @@
 #include "common.h"
 
 int main(int argc, char** argv) {
+    bool no_fuse = false;
+
     for (int i = 0; i < argc; i++) {
         if (std::string(argv[i]) == "--mountpoint" || std::string(argv[i]) == "-m") {
             if (i + 1 < argc) {
@@ -18,16 +20,36 @@ int main(int argc, char** argv) {
 
             break;
         }
+
+        if (std::string(argv[i]) == "--no-fuse" || std::string(argv[i]) == "-n") {
+            if (i + 1 < argc) {
+                no_fuse = true;
+                argc -= 1;
+
+                for (int j = i; j < argc; j++) {
+                    argv[j] = argv[j + 1];
+                }
+            }
+
+            break;
+        }
     }
 
-    if (TestConfig::inst().mountpoint.to_string().empty()) {
+    if (TestConfig::inst().mountpoint.to_string().empty() && !no_fuse) {
         throw std::runtime_error("No mountpoint provided");
     }
 
     testing::InitGoogleTest(&argc, argv);
+
+    if (no_fuse) {
+        testing::GTEST_FLAG(filter) = "-EncryptionVfs.*:VersioningVfs.*:CustomVfs.*";
+    }
+
     int test_results = RUN_ALL_TESTS();
 
-    Common::clean_mountpoint();
-    
+    if (!no_fuse) {
+        Common::clean_mountpoint();
+    }
+
     return test_results;
 }
